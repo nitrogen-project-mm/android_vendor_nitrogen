fbcc8ae5e4bab568600bc09c240071bae7bbec33
Valeriy <binder_z@mail.ru>
2016-06-25 20:16:24 +0400

    Renamed and removed unnecessary bootanimations

--- products/bootanimation.mk
+++ products/bootanimation.mk
@@ -14,29 +14,29 @@
 
 ifneq ($(filter nitrogen_geeb,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/geeb.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/768x1280.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nitrogen_geehrc,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/geehrc.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/768x1280.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nitrogen_mako,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/mako.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/768x1280.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nitrogen_hammerhead,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/hammerhead.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/1080x1920.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nitrogen_bullhead,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/hammerhead.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/1080x1920.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nitrogen_sprout4,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/sprout4.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/480x854.zip:system/media/bootanimation.zip
 endif
 ifneq ($(filter nitrogen_sprout8,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
-        vendor/nitrogen/prebuilt/common/bootanimation/sprout8.zip:system/media/bootanimation.zip
+        vendor/nitrogen/prebuilt/common/bootanimation/480x854.zip:system/media/bootanimation.zip
 endif
